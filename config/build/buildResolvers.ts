a6892457745462968f01927e89fa86793a5ab3d9
Stepashka <foraudo@mail.ru>
2023-02-06 11:29:06 +0500

    Архитектура FSD

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,5 +1,11 @@
-export function buildResolvers() {
+import { BuildOptions } from './types/config';
+
+export function buildResolvers({ paths }: BuildOptions) {
   return {
     extensions: ['.tsx', '.ts', '.js'],
+    preferAbsolute: false,
+    alias: {},
+    modules: [paths.src, 'node_modules'],
+    mainFiles: ['index'],
   };
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, { Suspense, useContext } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
+import { classNames } from 'helpers/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -15,8 +15,8 @@ const App = () => {
       <Link to="/about">About</Link>
       <Suspense fallback={<div>Loading ....</div>}>
         <Routes>
-          <Route path="/" element={<MainPageAsync />}></Route>
-          <Route path="/about" element={<AboutPageAsync />}></Route>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/about" element={<AboutPage />}></Route>
         </Routes>
       </Suspense>
     </div>
