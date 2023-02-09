c255da79baa45c232b84ffba93868396233fc16b
Stepashka <foraudo@mail.ru>
2023-02-09 17:36:28 +0500

    Sidebar. Layout приложения

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,17 @@ import { classNames } from 'shared/lib/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { useTheme } from './providers/ThemeProvider';
+import { Sidebar } from 'widgets/Sidebar';
 
 const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
