9815baaf0f4f83d8df474677dfc07efd8efb9600
Stepashka <foraudo@mail.ru>
2023-02-11 22:46:24 +0500

    Тестовая среда. Настраиваем Jest.

---
+++ src/shared/lib/classNames.test.ts
@@ -0,0 +1,38 @@
+import { classNames } from './classNames';
+
+describe(' unit test ', () => {
+    test('with only first param', () => {
+        expect(classNames('class')).toBe('class');
+    });
+    test('with additional class', () => {
+        const expected = 'class class1 class2';
+        expect(classNames(
+            'class',
+            {},
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+    test('with mods', () => {
+        expect(
+            classNames(
+                'class',
+                { required: true },
+            ),
+        ).toBe('class required');
+    });
+    test('with mods false params', () => {
+        const expected = 'class required';
+        expect(classNames(
+            'class',
+            { required: true, selected: false },
+        )).toBe(expected);
+    });
+    test('with mods undefined params', () => {
+        const expected = 'class selected';
+        expect(classNames(
+
+            'class',
+            { required: undefined, selected: true },
+        )).toBe(expected);
+    });
+});
--- src/shared/lib/classNames.ts
+++ src/shared/lib/classNames.ts
@@ -1,6 +1,10 @@
 type Mods = Record<string, boolean | string>;
 
-export const classNames = (cls: string, mods: Mods = {}, addentional: string[] = []): string => [
+export const classNames = (
+    cls: string,
+    mods: Mods = {},
+    addentional: string[] = [],
+): string => [
     cls,
     ...addentional.filter(Boolean),
     ...Object.entries(mods)
