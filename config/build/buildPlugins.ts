642885e855803ac609c86a0222176ff626dd0b7a
Stepashka <foraudo@mail.ru>
2023-02-19 14:11:56 +0500

    Скриншотные тесты. Loki. Регрессионное UI тестирование. CI pipeline. Автоматизация прогона тестов

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,28 +1,34 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
-import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BuildOptions } from './types/config';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
-export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:6].css',
-            chunkFilename: 'css/[name].[contenthash:6].css',
-        }),
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
-        new webpack.ProgressPlugin(),
+export function buildPlugins({
+  paths,
+  isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  const plugin = [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:6].css",
+      chunkFilename: "css/[name].[contenthash:6].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
 
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
 
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-          openAnalyzer: false
-        })
-    ];
+  if (isDev)
+    plugin.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new BundleAnalyzerPlugin({ openAnalyzer: false })
+    );
+
+  return plugin;
 }
