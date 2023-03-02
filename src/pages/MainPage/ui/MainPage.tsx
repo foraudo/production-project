709f06d58b01d54414e641fe46fc0696e978ba47
Stepashka <foraudo@mail.ru>
2023-03-02 22:29:27 +0500

    Husky. macos

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,4 @@
-/* eslint-disable i18next/no-literal-string */
-import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
